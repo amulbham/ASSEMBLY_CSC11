e02297f92f01e1786ddce8cae3b2faa06bd111b0
Amul Bham <amul.bham@gmail.com>
2015-10-26 16:24:09 -0700

    coded the midterm in C code, ready to be transferred to assembly

--- proj/midterm_c/main.cpp
+++ proj/midterm_c/main.cpp
@@ -10,197 +10,142 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-/*
- * 
- */
+void prob1();
+void prob2();
+void prob3();
 int main(int argc, char** argv) {
+    _menu:
+    unsigned int R0;
+    printf("Enter a number for the problem you would like to see\n"
+            "Problems 1-3 or 4 to exit\n");
+    scanf("%d",&R0);
     
-    cout<<"Problem 1: "<<endl<<endl;
+    if (R0 == 1){prob1(); goto _menu;}
+    else if (R0 == 2){prob2(); goto _menu;}
+    else if (R0 == 3){prob3(); goto _menu;}
+    else if (R0 == 4){goto _exit;}
+    else{goto _menu;}
+   
+    _exit:
+    return 0;
+
+}
+
+void prob1(){
+    printf("Problem 1:Gross Pay\n");
     //Declare Variables
-    unsigned int hrs;     //hours worked
-    unsigned int rtePy; //rate of pay 
-    unsigned int gPay;  //Gross pay
+    unsigned int R0;             //hours worked
+    unsigned int R1;            //rate of pay 
+    unsigned int R2;            //Gross pay
     
     //Initialize Variables - get user input
     printf("Please enter your rate of pay\n");
-    scanf(rtePy);
+    scanf("%d",&R1);
     printf("Enter the amount of hours worked\n");
-    scanf(hrs);
+    scanf("%d",&R0);
     
     //Determine pay rate accounting for overtime
     //hrs and rtePy are passed as parameters to the functions
     //Each should be a function that the program branches too based on the hours worked.
     
-    //So first should be cmp to 40, 50 and 60
-    unsigned int sTime = (rtePy*hrs); //given first twenty hours are fulfilled
-    unsigned int dTime = (rtePy*40)+ ((hrs-40)*(2*rtePy));
-    unsigned int tTime = (rtePy*40)+ (2*rtePy * 10) + (hrs-60)*(3*rtePy);
-    
-    
     //Use an if statement to determine which rate of pay the user falls in
-    if (hrs<=40){
-        goto sTime;
-    }else if(hrs>40 && hrs<=50){
-        goto dTime;
+    if (R0<=40){
+        goto _sTime;
+    }else if(R0>40 && R0<=50){
+        goto _dTime;
     }else{
-        goto tTime;
+        goto _tTime;
     }
-   //Output the gross pay of the user
-    cout<<fixed<<showpoint<<setprecision(2);
-    cout<<"Your Gross pay is $"<<gPay<<endl;
-    cout<<endl;
-
-     cout<<"In problem # 2"<<endl<<endl;
     
-    cout<<"\nThis program calculates your monthly Internet bill \n"
-        "and determines if switching to a higher tier will save \n"
-        "you money\n\n";   
+    //So first should be cmp to 40, 50 and 60
+    _sTime:
+    R2 = (R1*R0); //given first twenty hours are fulfilled
+    goto _output;
+    _dTime:
+    R2 = (R1*40)+ ((R0-40)*(2*R1));
+    goto _output;
+    _tTime:
+    R2 = (R1*40)+ (2*R1 * 10) + (R0-50)*(3*R1);
+    goto _output;
 
+   
+    //Output the gross pay of the user
+    _output:
+    printf("Your gross pay is $");
+    printf("%d",R2);
+    cout<<"\n";
+
+}
+void prob2(){
+    printf("In problem # 2: ISP Packages\n");
+    _prob2:
     //Declare Variables
-    int choice; //User input for which package they have
-    int hrs;    //User input for hours spent online
-    float mCost; //monthly charge for Internet service
-    float mSav; //money saved by switching to a new package 
-    int sav;   //sentinel value used to determine which package will save money
-    
-    
-    //Initialize variables - get user input
-    cout<<"Please select which tier of service you currently own (1-3)\n"<<endl;
-    cout<<"(1)  $16.85 per month, 5 hours access"<<endl;
-    cout<<"(2)  $23.85 per month, 15 hours access"<<endl;
-    cout<<"(3)  $29.55 per month unlimited access"<<endl;
-    cin>>choice;
-    
-    cout<<"How many hours do you typically spend using the Internet?"<<endl;
-    cin>>hrs;
+    char R0; //User input for which package they have
+    unsigned int R1 = 0;   //User input for hours spent online
+    unsigned int R2 = 0; //monthly charge for Internet service
    
-    //Determine tier pricing
-    float tier1=16.85;
-    float tier2=23.85;
-    float tier3=29.55;
-   
-    //Pricing for over charges 
-    float tier1a=(tier1+((hrs-5)*0.8)); //first overcharge rate for tier1
-    float tier1b=(tier1+(1*(hrs-20)+12)); //second overcharge rate for tier1
-    float tier2a=(tier2+((hrs-15)*0.65));//first overcharge rate for tier2
-    float tier2b=(tier2+(0.7*(hrs-25)+6.5));//second overcharge rate for tier2
+    printf("Please select which tier of service you currently own \n"
+    "(a)  $30 per month, 11 hours access\n"
+    "(b)  $35 per month, 22 hours access\n"
+    "(c)  $40 per month, 33 hours access\n");
+    //User inputs their choice of package that they currently own     
+    printf("Type in your choice here\n");
+    cin>>R0;
     
-    //Call the function to get monthly charge
-    mCost = getMonthlycharge(choice,hrs); //calculates users monthly charge
+    printf("How many hours did you spend using the Internet\n");
+    scanf("%d",&R1);
     
-    /*Use switch to compare monthly charge across all tiers
-      Switch used to narrow scope, if else statement used to address 
-      separate tiers within each package
-    */ 
-    switch(choice){
-        case 1: //package 1
-            if(tier1a>tier2 && hrs<=15){
-                    mSav = tier1a-tier2;
-                    sav = 2;break;
-            }else if (tier1a>tier2a && hrs>15 && hrs<=20){
-                mSav = tier1a-tier2a;
-                sav =2; break;
-            }else if (tier2a<tier3 && hrs>20 && hrs<=25){
-                mSav = tier1b - tier2a;
-                sav = 2; break;
-            }else if (tier2a>tier3 && hrs>20 && hrs<=25){
-                mSav = tier1b-tier3;
-                sav=3;break;
-            }else if (hrs>25){
-                mSav = tier1b-tier3;
-                sav=3;break;
-            }else{
-                sav = 0;
-            }break;
-        case 2: //package 2
-            if (hrs<=5){
-                mSav = tier2 - tier1;
-                sav = 1;
-                break;
-            }else if (hrs>5 && hrs<=15 && tier2>tier1a){
-                mSav = tier2 - tier1a;
-                sav = 1;
-                break;
-            }else if (hrs<=25 && hrs>15 && tier2a>tier3){
-                mSav = tier2a - tier3;
-                sav = 3;
-                break;    
-            }else if (tier2b>tier3 && hrs>25){
-                mSav = tier2b - tier3;
-                sav = 3;
-                break;
-            }else {
-                sav = 0;
-            }break;
-        case 3:  //package 3
-            if (hrs<=5){
-                mSav = tier3-tier1;
-                sav = 1;
-                break;
-            }else if (hrs>5 && hrs<=15 && tier2>tier1a){
-                mSav = tier3 - tier1a;
-                sav = 1;
-                break;
-            }else if (tier1a>tier2 && hrs>5 && hrs<=15 ){
-                mSav =tier3-tier2;
-                sav = 2;
-                 break;
-            }else if(tier3 > tier2a && hrs>15 && hrs<=25){
-                mSav = tier3 - tier2a;
-                sav = 2;
-                 break;
-            }else{
-                sav = 0;
-            }break;}
-                
-    //Output the monthly charge 
-    cout<<"\nYour current monthly bill is $"<<mCost<<"\n";
+    if (R0 == 'a'){goto _tierA;}
+    else if (R0 == 'b'){goto _tierB;}
+    else if (R0 == 'c'){goto _tierC;}
+    else{goto _prob2;}
     
-    //Output monthly savings if user could save money switching to new package
-    cout<<fixed<<showpoint<<setprecision(2);
+    _tierA:
+    R2 = 30;
+    if (R1>11 && R1<=22){R2+=(R1-11)*3;}
+    else if (R1>22){R2+=(3*11)+((R1-22)*6);}
+    goto _output2; 
+    _tierB:
+    R2 = 35;
+    if(R1>22 && R1<=44){R2+=(R1-22)*2;}
+    else if (R1>44){R2+=(22*2)+((R1-44)*4);}
+    goto _output2;
+    _tierC:
+    R2=40;
+    if (R1>33 && R1<=66){R2+=(R1-33)*1;}
+    else if (R1>66){R2+=(33*1)+((R1-66)*2);}
+    goto _output2;
     
-    if (sav == 1){
-        cout<<"You can save an additional $"<<mSav<<" if you switch "
-                "to the Tier 1 package \n\n\n" ;
-    }else if (sav == 2){
-        cout<<"You can save an additional $"<<mSav<<" if you switch "
-                "to the Tier 2 package \n\n\n";
-    }else if (sav == 3){
-        cout<<"You can save an additional $"<<mSav<<" if you switch "
-                "to the Tier 3 package \n\n\n" ;
-    }else{
-        cout<<"No savings can be made, you are paying for the right tier\n\n\n";
-    }
+    _output2:
+    //Output the monthly charges to the user
+    printf("Your total monthly charges, including extra fees is $");
+    printf("%d",R2);
+    cout<<"\n";
 
 }
 
-float getMonthlycharge(int choice,int hrs){
-    float mCost;
-    //Set tier pricing
-    float tier1=16.85;
-    float tier2=23.85;
-    float tier3=29.55;
-    //Pricing for over charges 
-    float tier1a=(tier1+((hrs-5)*0.8));
-    float tier1b=(tier1+(1*(hrs-20)+12));
-    float tier2a=(tier2+((hrs-15)*0.65));
-    float tier2b=(tier2+(0.7*(hrs-25))+6.5);
-    //create if else statement to determine which tier should be applied
-    if (choice == 1){
-        if (hrs<=5)mCost=tier1;
-        else if(hrs>5 && hrs<=20)mCost=tier1a;
-        else if(hrs>20)mCost=tier1b;
-    }else if (choice == 2){
-        if (hrs<=15)mCost=tier2;
-        else if(hrs>15 && hrs<=25)mCost=tier2a;
-        else if(hrs>25)mCost=tier2b;
-    }else if (choice == 3){
-        mCost=tier3;
-    }
-    //Exist stage return monthly cost
-    return mCost;
-
+void prob3(){
+    //Declare variables
+    unsigned int R0=0;   //value of the # requested
+    unsigned int R1=1;   //previous value
+    unsigned int R2=1;   //current value
+    unsigned int R3=0;  //# of the sequence
+    
+    printf("In problem # 3: Fibonacci Sequence\n");
+    printf("Which term of the Fibonacci Sequence would you like to see?\n");
+    cin>>R3;
+    //Loop until the fib sequence reaches the users desired number
+    _loop:
+    R0 = R1 + R2;
+    R1 = R2;
+    R2 = R0;
+    R3--;
+    if (R3!=2)goto _loop;
+    //Print the Fibonacci sequence number 
+    printf("%d",R0);
+    cout<<"\n";
 }
